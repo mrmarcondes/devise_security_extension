599e563839f7c049ce7a307c3fc4aa204f956e32
Marco Rojo <mrmarcondes@gmail.com>
2016-11-07 12:57:27 -0200

    change PasswordArchivable#password_archive_included

--- lib/devise_security_extension/models/old_password.rb
+++ lib/devise_security_extension/models/old_password.rb
@@ -2,5 +2,12 @@ class OldPassword
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  field :encrypted_password, :type => String, :null => false
+  field :password_salt, :type => String
+  field :password_archivable_id, :type => Integer
+  field :password_archivable_type, :type => String, :null => false
+
+  attr_accessible :encrypted_password
+
   belongs_to :password_archivable, polymorphic: true
 end
